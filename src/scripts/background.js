da683eaece223a2b820fa907c42f0647aed0f0af
Jinxing Lin <172601673@qq.com>
2019-05-25 00:45:00 +0800

    add message listener

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -9,39 +9,95 @@ import { local, sync } from "../utils/storage";
 
 const chrome = window.chrome;
 
-const defaultMatcher = new CombinedMatcher();
-defaultPacRules.forEach(rule => {
-  defaultMatcher.add(Filter.fromText(rule));
-});
+(async () => {
+  let lastHost; // 用于避免事件的重复触发
+  let currentUrlHasProxy = false; // 判断点击 browser_action_icon 时，该进行的操作
 
-chrome.browserAction.onClicked.addListener(() => {
-  // @TODO
-});
+  // chrome 数据加载加载
+  let { enable, proxy, userRulesSerial } = await local.get([
+    "enable",
+    "proxy",
+    "userRulesSerial"
+  ]);
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.runtime.openOptionsPage();
-});
+  // 图标
+  if (!enable) chrome.browserAction.setIcon({ path: "proxy-unable.png" });
 
-let lastHost;
-const handleTagUpdatedAndChanged = async () => {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-    const location = new URL(tabs[0].url);
-    const { href: url, host } = location;
-    if (lastHost === host) return;
-    lastHost = host;
-    // @TODO
-    const dfilter = defaultMatcher.matchesAny(url, host);
-    if (dfilter instanceof BlockingFilter) {
-      chrome.browserAction.setIcon({ path: "proxy-enable.png" });
+  // default matcher 初始化
+  const defaultMatcher = new CombinedMatcher();
+  defaultPacRules.forEach(rule => {
+    defaultMatcher.add(Filter.fromText(rule));
+  });
+  // user matcher 初始化
+  const userRules = (userRulesSerial || "").split(";").filter(rule => !!rule);
+  const userRulesMatcher = new CombinedMatcher();
+  userRules.forEach(rule => {
+    userRulesMatcher.add(Filter.fromText(rule));
+  });
+
+  chrome.browserAction.onClicked.addListener(() => {
+    // 未开启，打开选项页
+    if (!enable) chrome.runtime.openOptionsPage();
+    if (currentUrlHasProxy) {
+      // 取消代理
     } else {
-      chrome.browserAction.setIcon({ path: "proxy-unable.png" });
+      // 启用代理
     }
   });
-};
 
-chrome.windows.onRemoved.addListener(function(windowId) {
-  // @TODO async
-});
+  chrome.runtime.onInstalled.addListener(() => {
+    chrome.runtime.openOptionsPage();
+  });
 
-chrome.tabs.onUpdated.addListener(handleTagUpdatedAndChanged);
-chrome.tabs.onSelectionChanged.addListener(handleTagUpdatedAndChanged);
+  const handleTagUpdatedAndChanged = async () => {
+    if (!enable) {
+      chrome.browserAction.setIcon({ path: "proxy-unable.png" });
+      return;
+    }
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+      const location = new URL(tabs[0].url);
+      const { href: url, host } = location;
+      if (lastHost === host) return;
+      lastHost = host;
+      const ufilter = userRulesMatcher.matchesAny(url, host);
+      const dfilter = defaultMatcher.matchesAny(url, host);
+      if (ufilter instanceof BlockingFilter) {
+        chrome.browserAction.setIcon({ path: "proxy-enable.png" });
+        currentUrlHasProxy = true;
+      } else if (ufilter instanceof WhitelistFilter) {
+        chrome.browserAction.setIcon({ path: "proxy-unable.png" });
+        currentUrlHasProxy = false;
+      } else if (dfilter instanceof BlockingFilter) {
+        chrome.browserAction.setIcon({ path: "proxy-enable.png" });
+        currentUrlHasProxy = true;
+      } else {
+        chrome.browserAction.setIcon({ path: "proxy-unable.png" });
+        currentUrlHasProxy = false;
+      }
+    });
+  };
+  chrome.tabs.onUpdated.addListener(handleTagUpdatedAndChanged);
+  chrome.tabs.onSelectionChanged.addListener(handleTagUpdatedAndChanged);
+
+  chrome.windows.onRemoved.addListener(() => {
+    // @TODO async
+  });
+
+  // options 页面的数据更新
+  chrome.runtime.onMessage.addListener(request => {
+    const { type, data } = request;
+    switch (type) {
+      case "SWITCH":
+        enable = data;
+        chrome.browserAction.setIcon(
+          enable ? "proxy_enable.png" : "proxy_unable.png"
+        );
+        // @TODO cancel proxy, sync data
+        break;
+      case "PROXY":
+        proxy = data;
+        // @TODO update proxy config，sync data
+        break;
+    }
+  });
+})();
---
+++ src/utils/proxy-helpper.js
@@ -0,0 +1,1 @@
+const chrome = window.chrome;
