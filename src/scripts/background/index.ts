0ce918d68752785571eeb42412e5732626efb00a
Jinxing Lin <172601673@qq.com>
2020-06-02 00:12:27 +0800

    子域名的情况

--- src/scripts/background/index.ts
+++ src/scripts/background/index.ts
@@ -10,7 +10,7 @@ import { addRule, deleteRule } from "@/actions/rule";
 const store = queryStore(true);
 
 async function handleTabsActivated() {
-  const tabs: chrome.tabs.Tab[] = await new Promise((resolve) => {
+  const tabs: chrome.tabs.Tab[] = await new Promise(resolve => {
     chrome.tabs.query({ active: true, currentWindow: true }, resolve);
   });
   if (!tabs.length) return;
@@ -22,7 +22,7 @@ async function handleTabsActivated() {
   }
 
   const state = store.getState();
-  const userRules = state.rule.map((i) => i.pattern);
+  const userRules = state.rule.map(i => i.pattern);
 
   let curFilter = queryFilter(curTab.url, userRules);
   if (!curFilter) {
@@ -35,8 +35,7 @@ async function handleTabsActivated() {
     chrome.browserAction.setIcon({ path: whitelistIcon });
   } else {
     chrome.browserAction.setIcon({
-      path:
-        state.proxy.gfwMode === GFWMode.BLOCKING ? whitelistIcon : blockingIcon,
+      path: state.proxy.gfwMode === GFWMode.BLOCKING ? whitelistIcon : blockingIcon
     });
   }
 }
@@ -56,7 +55,7 @@ function handleTabsUpdated(
 chrome.tabs.onUpdated.addListener(handleTabsUpdated);
 
 async function handleBrowserActionClicked() {
-  const tabs: chrome.tabs.Tab[] = await new Promise((resolve) => {
+  const tabs: chrome.tabs.Tab[] = await new Promise(resolve => {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, resolve);
   });
   const curTab = tabs[0];
@@ -64,33 +63,25 @@ async function handleBrowserActionClicked() {
     return;
   }
   const state = store.getState();
-  const userRules = state.rule.map((i) => i.pattern);
+  const userRules = state.rule.map(i => i.pattern);
   const gfwRules = state.proxy.gfwList;
 
-  let curFilter = queryFilter(curTab.url, userRules);
   const { hostname } = new URL(curTab.url);
-  // 存在于用户规则中
-  if (curFilter instanceof BlockingFilter) {
-    store.dispatch(deleteRule(curFilter.text));
-    store.dispatch(addRule("@@" + hostname));
-  } else if (curFilter instanceof WhitelistFilter) {
-    store.dispatch(deleteRule(curFilter.text));
-    store.dispatch(addRule(hostname));
-  }
-  // 存在于 gfwlist 中
-  if (!curFilter && (curFilter = queryFilter(curTab.url, gfwRules))) {
-    if (curFilter instanceof BlockingFilter) {
-      store.dispatch(addRule("@@" + hostname));
-    } else if (curFilter instanceof WhitelistFilter) {
-      store.dispatch(addRule(hostname));
-    }
-  }
-  // 即不存在 用户规则 中，又不存在于 gfwlist 中
-  if (!curFilter) {
-    if (state.proxy.gfwMode === GFWMode.BLOCKING) {
-      store.dispatch(addRule(hostname));
-    } else {
-      store.dispatch(addRule("@@" + hostname));
+  const pattern = userRules.find(rule => rule.replace(/^@@/, "") === hostname);
+  // 存在对应的用户规则
+  if (pattern) {
+    store.dispatch(deleteRule(pattern));
+    if (pattern.startsWith("@@")) store.dispatch(addRule(pattern.replace(/^@@/, "")));
+    else store.dispatch(addRule("@@" + pattern));
+  } else {
+    let filter = queryFilter(curTab.url, gfwRules);
+    // 存在于 gfwlist 中
+    if (filter instanceof BlockingFilter) store.dispatch(addRule("@@" + hostname));
+    else if (filter instanceof WhitelistFilter) store.dispatch(addRule(hostname));
+    // 即不存在 用户规则 中，又不存在于 gfwlist 中
+    else if (!filter) {
+      if (state.proxy.gfwMode === GFWMode.BLOCKING) store.dispatch(addRule(hostname));
+      else store.dispatch(addRule("@@" + hostname));
     }
   }
 
